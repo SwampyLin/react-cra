d8fa67bf87aecb8f8bb059510055d8d75d2849fa
SwampyLin <redze801201@hotmail.com.tw>
2023-06-17 00:34:16 +0800

    git init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+//外部套件
+
+import logo from './assets/logo.svg'
+import './assets/App.css'
+import Input from './components/Input'
+import './assets/all.scss'
 
 function App() {
+  const [text, setText] = useState('')
+  const onChangeHandler = (e) => {
+    setText(e.target.value)
+  }
+  useEffect(() => {
+    ;(async () => {
+      const path = process.env.REACT_APP_PATH
+      const result = await axios.get(path)
+      console.log(result)
+    })()
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className='App'>
+      <header className='App-header'>
+        <img src={logo} className='App-logo' alt='logo' />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Debitis,
+          quibusdam? Voluptatum, reprehenderit impedit soluta voluptates, illum
+          voluptate, totam obcaecati ab assumenda nesciunt ratione et officia
+          quidem aliquid? Aspernatur, animi amet!
         </p>
         <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+          className='App-link'
+          href='https://reactjs.org'
+          target='_blank'
+          rel='noopener noreferrer'
         >
           Learn React
         </a>
+        <button type='button' className='btn btn-primary'>
+          Primary
+        </button>
+        {text}
+        <Input
+          id='sampleText'
+          text='這是一個input'
+          value={text}
+          onChangeHandler={onChangeHandler}
+        />
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
