ff6584b8858a6d48dc933eeddc79cfa27635e8f0
SwampyLin <redze801201@hotmail.com.tw>
2023-06-21 17:24:14 +0800

    shoppingCart
    standard

--- src/App.js
+++ src/App.js
@@ -1,55 +1,27 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useReducer } from 'react'
 import axios from 'axios'
 //外部套件
-
-import logo from './assets/logo.svg'
-import './assets/App.css'
-import Input from './components/Input'
-import './assets/all.scss'
+import Navbar from './components/Navbar'
+import Products from './components/Products'
+import Cart from './components/Cart'
+import { CartContext, cartReducer, cartInit } from './store'
 
 function App() {
-  const [text, setText] = useState('')
-  const onChangeHandler = (e) => {
-    setText(e.target.value)
-  }
-  useEffect(() => {
-    ;(async () => {
-      const path = process.env.REACT_APP_PATH
-      const result = await axios.get(path)
-      console.log(result)
-    })()
-  }, [])
-
+  const reducer = useReducer(cartReducer, cartInit)
   return (
-    <div className='App'>
-      <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
-        <p>
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Debitis,
-          quibusdam? Voluptatum, reprehenderit impedit soluta voluptates, illum
-          voluptate, totam obcaecati ab assumenda nesciunt ratione et officia
-          quidem aliquid? Aspernatur, animi amet!
-        </p>
-        <a
-          className='App-link'
-          href='https://reactjs.org'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          Learn React
-        </a>
-        <button type='button' className='btn btn-primary'>
-          Primary
-        </button>
-        {text}
-        <Input
-          id='sampleText'
-          text='這是一個input'
-          value={text}
-          onChangeHandler={onChangeHandler}
-        />
-      </header>
-    </div>
+    <CartContext.Provider value={reducer}>
+      <Navbar></Navbar>
+      <div className='container mt-4'>
+        <div className='row'>
+          <div className='col-md-7'>
+            <Products></Products>
+          </div>
+          <div className='col-md-5'>
+            <Cart></Cart>
+          </div>
+        </div>
+      </div>
+    </CartContext.Provider>
   )
 }
 
